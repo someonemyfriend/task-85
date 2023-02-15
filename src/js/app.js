8e06c6e6cf2e00c9ed50f6fb3cc3c15ca01edf4f
Vasilia Staykova <someonemyfriend@gmail.com>
2023-02-15 16:26:59 +0200

    Add files via upload

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,7 @@ window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
   const pokemonCount = 10;
   const ul = document.querySelector("ul");
-  fetch('https://pokeapi.co/api/v2/pokemon/?limit={pokemonCount}')
+  fetch('https://pokeapi.co/api/v2/pokemon/?limit=${pokemonCount}')
     .then((response) => response.json())
     .then((data) => {
       data.results.forEach((pokemon) => {
