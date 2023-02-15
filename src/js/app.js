114a6d8793ed43a4c31e132ad21bb61e9864b396
Vasilia Staykova <someonemyfriend@gmail.com>
2023-02-15 16:27:19 +0200

    Add files via upload

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,15 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
+  const pokemonCount = 10;
   const ul = document.querySelector("ul");
+  fetch('https://pokeapi.co/api/v2/pokemon/?limit=${pokemonCount}')
+    .then((response) => response.json())
+    .then((data) => {
+      data.results.forEach((pokemon) => {
+        let li = document.createElement('li');
+        li.innerText = pokemon.name;
+        ul.appendChild(li);
+      });
+    });
 });
